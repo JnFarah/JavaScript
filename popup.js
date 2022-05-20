f33b7907d77bb87d5d7fef3cfc76ce73de02a760
JnFarah <farah.mebarki@gmail.com>
2022-05-20 11:01:02 +0200

    first Js exercices

---
+++ popup.js
@@ -0,0 +1,20 @@
+let affiche = document.getElementById('affiche');
+let img = affiche.firstElementChild;
+// console.log(img);
+let thumbCol = document.getElementsByClassName('thumb');
+// console.log(thumbCol);
+let thumbs = Array.from(thumbCol);
+// console.log(thumbs);
+
+//boucle forEach toujours pareil maggle
+thumbs.forEach(function(thumb){
+    let imgUrl=thumb.src;
+    thumb.onclick=function(){
+        img.src = imgUrl;
+        affiche.style.visibility = 'visible';
+    };
+});
+
+affiche.onclick = function(){
+    affiche.style.visibility = 'hidden';
+};
